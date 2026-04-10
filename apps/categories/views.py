8c55275cef740262eef7400021003dbb2d18fb9f
Pedro Kunz <pedrokunz094@gmail.com>
2026-04-10 20:13:25 -0300

    feature: finalizando roteiro 5.3

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -12,7 +12,15 @@ def add_category(request):
             f = form.save(commit=False)
             f.save()
             form.save_m2m()
-            return redirect('core:home')
+            return redirect('categories:list_categories')
     form = CategoryForm()
     context['form'] = form
+    return render(request, template_name, context)
+
+def list_categories(request):
+    template_name = 'categories/list_categories.html'
+    categories = Category.objects.filter()
+    context = {
+        'categories': categories
+    }
     return render(request, template_name, context)
