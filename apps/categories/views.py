2911dcd291c953455465bd26c4176757ba998d28
Pedro Kunz <pedrokunz094@gmail.com>
2026-04-10 20:00:47 -0300

    feature: finalizando o roteiro 5.2

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CategoryForm
+from .models import Category
 
 # Create your views here.
+def add_category(request):
+    template_name = 'categories/add_category.html'
+    context = {}
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            f = form.save(commit=False)
+            f.save()
+            form.save_m2m()
+            return redirect('core:home')
+    form = CategoryForm()
+    context['form'] = form
+    return render(request, template_name, context)
